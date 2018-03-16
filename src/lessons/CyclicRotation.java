36a68ba4160997743b90c5a14efb4a868ca61c03
Osagie Omonzokpia <osagieomon@Osagies-MacBook-Pro.local>
2018-03-16 09:47:39 +0100

    Odd occurences

--- src/lessons/CyclicRotation.java
+++ src/lessons/CyclicRotation.java
@@ -5,8 +5,6 @@
  */
 package lessons;
 
-import java.util.Arrays;
-
 /**
  *
  * @author osagieomon
@@ -18,39 +16,49 @@ public class CyclicRotation {
         int[] newArray = new int[n];
 
         // Method 1
-//        for (int i = 0; i < k; i++) {
-//            for (int j = 0; j < n; j++) {
-//                if (j+1 < n) {
-//                    newArray[j+1] = A[j];
-//                } else {
-//                    newArray[0] = A[j];
+//        if (n == 1) {
+//            return A;
+//        } else {
+//            for (int i = 0; i < k; i++) {
+//                for (int j = 0; j < n; j++) {
+//                    if (j + 1 < n) {
+//                        newArray[j + 1] = A[j];
+//                    } else {
+//                        newArray[0] = A[j];
+//                    }
 //                }
+//
+//                A = Arrays.copyOf(newArray, n);
 //            }
-//            
-//            A = Arrays.copyOf(newArray, n);
+//
+//            return newArray;
 //        }
 
         // Method 2
-        for (int i = 0; i < n; i++) {
-            int diff = k - n;
-            
-            int newIndex = i + diff;
-            
-            if(newIndex < 0) {
-                newArray[n + newIndex] = A[i];
-            } else if(newIndex > (n - 1)) {
-                newArray[n - newIndex] = A[i];
-            } else {
-                newArray[newIndex] = A[i];
-            }
-        }
+        if (n == 1) {
+            return A;
+        } else {
+            for (int i = 0; i < n; i++) {
+                int diff = k - n;
 
-        return newArray;
+                int newIndex = i + diff;
+
+                if (newIndex < 0) {
+                    newArray[n + newIndex] = A[i];
+                } else if (newIndex > (n - 1)) {
+                    newArray[n - newIndex] = A[i];
+                } else {
+                    newArray[newIndex] = A[i];
+                }
+            }
+
+            return newArray;
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = solution(new int[]{3}, 5);
-        
+
         for (int num : arr) {
             System.out.print(num + " ");
         }
---
+++ src/lessons/OddOccurrencesInArray.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lessons;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author osagieomon
+ */
+public class OddOccurrencesInArray {
+    
+    static int solution(int[] A) {
+        int ar_size = A.length;
+        int i;
+        int res = 0;
+        
+        for (i = 0; i < ar_size; i++) {
+            res = res ^ A[i];
+        }
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        int num = solution(new int[]{9, 3, 9, 3, 9, 7, 9});
+
+        System.out.println(num);
+        
+    }
+}
